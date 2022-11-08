0f5e94fdee28e54684a6982a1aade51e76d15943
Leonardo <leonardohenriquetubero@gmail.com>
2022-11-08 17:32:22 -0300

    Update department

--- src/db/DB.java
+++ src/db/DB.java
@@ -42,7 +42,7 @@ public class DB {
 	}
 	
 	private static Properties loadProperties() {
-		try(FileInputStream fs = new FileInputStream("C:\\Java\\Eclipse\\eclipse\\Workspace Eclipse\\jdbc1\\src\\db.properties")){
+		try(FileInputStream fs = new FileInputStream("C:\\temp\\ws-eclipse\\workshop-javafx-jdbc\\db.properties")){
 			Properties props = new Properties();
 			props.load(fs);		
 			return props;
